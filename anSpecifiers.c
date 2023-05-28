a5f718b48d95bb2ccf8404589b81ac993affb77e
mhandie <anokpara@yahoo.com>
2023-05-28 08:42:03 -0700

    all files

--- anSpecifiers.c
+++ anSpecifiers.c
@@ -1,6 +1,48 @@
 #include "main.h"
-#include <stdarg.h>
-#include <stdarg.h>
+
+/**
+ * print_char - character
+ * @list: arguments passed
+ * Return: characters printed
+ */
+int print_char(va_list list)
+{
+_write_char(va_arg(list, int));
+return (1);
+}
+
+/**
+ * print_string - string
+ * @list: arguments passed
+ * Return: characters printed
+ */
+int print_string(va_list list)
+{
+char *str = va_arg(list, char *);
+
+int len = 0;
+if (str == NULL)
+str = "(null)";
+while (*str != '\0')
+{
+_write_char(*str);
+str++;
+len++;
+}
+return (len);
+}
+
+/**
+ * print_percent - percent
+ * @list: arguments passed
+ * Return: characters printed
+ */
+int print_percent(va_list list)
+{
+(void)list;
+_write_char('%');
+return (1);
+}
 
 /**
  * print_integer - prints integer
@@ -19,16 +61,16 @@ len++;
 num = -num;
 }
 len += print_number(num);
-return len;
+return (len);
 }
 
 /**
- * unsigned_integer - prints unsigned integers
- * @list: list of arguments passed
- * Return: number of characters printed
+ * unsigned_integer - unsigned integers
+ * @list: arguments passed
+ * Return: characters printed
  */
 int unsigned_integer(va_list list)
 {
 unsigned int num = va_arg(list, unsigned int);
-return print_unsgined_number(num);
+return (print_unsgined_number(num));
 }
