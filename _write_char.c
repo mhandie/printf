2d939795df1265c8a2ba0c5bd13786593afe59ef
NanaAbena <nanykyeraa@gmail.com>
2023-05-28 03:43:09 -0700

    Writechar corrected

--- _write_char.c
+++ _write_char.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * _putchar - prints a character
+ * _write_char - prints a character
  * @c: The character to print
  *
  * Return: 1 on a success
  * On error, -1 is returned
  */
 
-int _putchar(char c)
+int _write_char(char c)
 {
 	return (write(1, &c, 1));
 }
