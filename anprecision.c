beda47a9a715fe9b26e3de838778f4ea28c1554c
mhandie <anokpara@yahoo.com>
2023-05-30 13:16:49 -0700

    all files

--- anprecision.c
+++ anprecision.c
@@ -8,7 +8,7 @@
  *
  * Return: Precision.
  */
-int get_precision(const char *format, int *i, va_list list)
+int anprecision(const char *format, int *i, va_list list)
 {
 	int curr_i = *i + 1;
 	int precision = -1;
